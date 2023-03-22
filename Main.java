40284d019547adb544c36d20b55d9429197093cc
michal169751 <127328268+michal169751@users.noreply.github.com>
2023-03-22 15:35:58 +0000

    2_1

--- Main.java
+++ Main.java
@@ -1,9 +1,10 @@
 class Main {
   public static void main(String[] args) {
-    String[] Hello = "Hello World";
-    for (int i=0; i<Hello.length(); i++){
-      system.out.println(Hello.charAt(j);)
+    String str = "Hello World";
+    for (int i=0; i<str.length(); i++)
+    {
+      System.out.println(str.charAt(i));
     }
-    System.out.println(" ");
+    System.out.println("");
   }
 }
