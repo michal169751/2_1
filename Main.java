fc6550b6265c67502c00a7d20617f28f2cb34d06
michal169751 <127328268+michal169751@users.noreply.github.com>
2023-03-22 15:26:05 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,9 @@
+class Main {
+  public static void main(String[] args) {
+    String[] Hello = "Hello World";
+    for (int i=0; i<Hello.length(); i++){
+      system.out.println(Hello.charAt(j);)
+    }
+    System.out.println(" ");
+  }
+}
